00ae15b7208b8a26859bd362cd9ece5b563a665f
Fireflyest <fireflyest@qq.com>
2024-06-23 10:46:08 +0800

    文本工具类

---
+++ src/main/java/io/fireflyest/spigot/emberlib/util/StringUtils.java
@@ -0,0 +1,14 @@
+package io.fireflyest.spigot.emberlib.util;
+
+/**
+ * 文本工具类
+ * @author Fireflyest
+ * @since 1.0
+ */
+public final class StringUtils {
+    
+    private StringUtils() {
+        // 
+    }
+
+}
