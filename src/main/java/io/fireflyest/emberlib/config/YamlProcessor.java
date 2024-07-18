8514cf6cb1623db92b49154f27a34f689a3f9c87
Fireflyest <fireflyest@qq.com>
2024-07-18 21:21:29 +0800

    写入测试

--- src/main/java/io/fireflyest/emberlib/config/YamlProcessor.java
+++ src/main/java/io/fireflyest/emberlib/config/YamlProcessor.java
@@ -1,5 +1,7 @@
 package io.fireflyest.emberlib.config;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Messager;
@@ -8,6 +10,9 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardLocation;
+
 import org.bukkit.configuration.file.YamlConfiguration;
 
 /**
@@ -22,12 +27,14 @@ public class YamlProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         final Messager messager = processingEnv.getMessager();
-        messager.printMessage(Diagnostic.Kind.NOTE, "Processing database dao...");
+        messager.printMessage(Diagnostic.Kind.ERROR, "Processing database dao...");
         // 当前处理器支持的所有注解种类
         for (TypeElement typeElement : annotations) {
             // 获得被该注解声明的元素
             for (Element element : roundEnv.getElementsAnnotatedWith(typeElement)) {
-                messager.printMessage(Diagnostic.Kind.NOTE, element.getSimpleName());
+                messager.printMessage(Diagnostic.Kind.ERROR, element.getSimpleName());
+                YamlGenerator generator = new YamlGenerator("lang/default.yml");
+                generator.write();
             }
         }
         return true;
@@ -48,6 +55,23 @@ public class YamlProcessor extends AbstractProcessor {
 
         public YamlGenerator(String targetFile) {
             this.targetFile = targetFile;
+        }
+
+        public void write() {
+            JavaFileObject source = null;
+            try {
+                source = processingEnv.getFiler().createSourceFile("io.fireflyest.relatelock.config");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (source != null) {
+                try (Writer writer = source.openWriter()) {
+                    writer.write("//测试");
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         
     }
