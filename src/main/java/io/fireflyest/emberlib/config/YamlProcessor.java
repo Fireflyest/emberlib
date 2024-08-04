27551fc68acccfa078c4f8749d91b99fb447eabe
Fireflyest <fireflyest@qq.com>
2024-08-05 00:42:34 +0800

    配置处理使用其他方案

--- src/main/java/io/fireflyest/emberlib/config/YamlProcessor.java
+++ src/main/java/io/fireflyest/emberlib/config/YamlProcessor.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
@@ -12,11 +11,6 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import com.google.auto.service.AutoService;
-import com.sun.tools.javac.api.JavacTrees;
-import com.sun.tools.javac.tree.TreeMaker;
-import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.Names;
-import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import io.fireflyest.emberlib.config.annotation.Yaml;
 
 /**
@@ -29,19 +23,9 @@ import io.fireflyest.emberlib.config.annotation.Yaml;
 @SupportedAnnotationTypes("io.fireflyest.emberlib.config.annotation.Yaml")
 public class YamlProcessor extends AbstractProcessor {
 
-    private Messager messager;   // 用来在编译期打印日志
-    private JavacTrees trees;    // 提供了待处理的抽象语法树
-    private TreeMaker treeMaker; // 封装了创建AST节点的一些方法
-    private Names names;         // 提供了创建标识符的方法
-
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
-        final Context context = ((JavacProcessingEnvironment) processingEnv).getContext();
-        this.messager = processingEnv.getMessager();
-        this.trees = JavacTrees.instance(processingEnv);
-        this.treeMaker = TreeMaker.instance(context);
-        this.names = Names.instance(context);
     }
 
     @Override
@@ -59,7 +43,11 @@ public class YamlProcessor extends AbstractProcessor {
      */
     private void processElement(@Nonnull Element element) {
         final Yaml yaml = element.getAnnotation(Yaml.class);
-        final YamlGenerator generator = new YamlGenerator(processingEnv, element, yaml.value());
+        final YamlGenerator generator = new YamlGenerator(
+            processingEnv, 
+            (TypeElement) element, 
+            yaml.value()
+        );
 
         element.accept(generator, null);
 
@@ -69,5 +57,5 @@ public class YamlProcessor extends AbstractProcessor {
             e.printStackTrace();
         }
     }
-    
+
 }
